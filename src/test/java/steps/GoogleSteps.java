2527f9f6b3f688db7c46019aab13ddbaccb15cea
Santiago <santiagocastanonarvizu@gmail.com>
2022-04-21 14:12:14 -0500

    initial commit

---
+++ src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,26 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSteps {
+    @Given("^I am on the Google search page$")
+    public void navigateToGoogle(){
+
+    }
+    @When("^I enter a search criteria$")
+    public void enterSearchCriteria(){
+
+    }
+    @And("^click on the search button$")
+    public void clickSearchButton(){
+
+    }
+    @Then("^the results match the criteria$")
+    public void validateResult(){
+
+
+    }
+}
